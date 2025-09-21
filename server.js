92130873a148c1ee93943cfec837dcad4e17294e
Kranthi <vkranthi237@gmail.com>
2025-09-21 15:31:48 +0530

    Updated

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ const PORT = process.env.PORT || 3000;
 
 // ---------- MIDDLEWARE ----------
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-app.use(
-  session({
-    secret: "your_secret_key", // change this to something secure
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false } // set true only if HTTPS
-  })
-);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+}));
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
